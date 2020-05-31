29c84eba254980f4961a4df659c9342be0fa1ae7
Georgi Stankov <vangog06406@gmail.com>
2020-05-31 22:50:08 +0300

    SignalR

---
+++ SignalArgh/SignalArgh/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalArgh.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(string date, string message)
+        {
+            await this.Clients.All.SendAsync("messageReceived", date, message);
+        }
+    }
+}
